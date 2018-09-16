5fee2c37639c32f6027c9212e4cffb9141424874
David Liu <yidadavidliu@gmail.com>
2018-09-15 23:32:58 -0700

    GroupInfo package created

--- app/src/main/java/project/lumohacks/psychologyreport/groupInfo/Group.java
+++ app/src/main/java/project/lumohacks/psychologyreport/groupInfo/Group.java
@@ -1,8 +1,55 @@
 package project.lumohacks.psychologyreport.groupInfo;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by David Liu on 9/15/2018.
  */
 
 public class Group {
+    private List<Member> groupMembers;
+    private String clientName;
+    private int clientID;
+    private int groupID;
+
+    public Group(String clientName, int clientID, int groupID) {
+        groupMembers = new ArrayList<>();
+        this.clientName = clientName;
+        this.clientID = clientID;
+        this.groupID = groupID;
+    }
+
+    public void addMember(MemberType memberType, String name, String email, int phoneNumber,
+                          String relationWithClient) {
+
+        Member memberToAdd = new Member(memberType, name, email, phoneNumber, relationWithClient);
+    }
+
+    public void removeMember(String memberName) {
+
+        Iterator<Member> m = groupMembers.iterator();
+        while (m.hasNext()) {
+            Member member = m.next();
+            if (member.getName().equals(memberName)) {
+                m.remove();
+            }
+        }
+    }
+
+    // getters
+    public String getClientName() {
+        return clientName;
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public int getGroupID() {
+        return groupID;
+    }
 }
---
+++ app/src/main/java/project/lumohacks/psychologyreport/groupInfo/Member.java
@@ -0,0 +1,44 @@
+package project.lumohacks.psychologyreport.groupInfo;
+
+import java.util.Map;
+
+/**
+ * Created by David Liu on 9/15/2018.
+ */
+
+public class Member {
+    private MemberType memberType;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String relationWithClient;
+
+    public Member(MemberType memberType, String name, String email, int phoneNumber,
+                       String relationWithClient) {
+        this.memberType = memberType;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.relationWithClient = relationWithClient;
+    }
+
+    // make a
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemberEmail() {
+        return email;
+    }
+
+    public int getMemberPhoneNumber() {
+        return  phoneNumber;
+    }
+
+    public String getMemberRelationWithClient() {
+        return relationWithClient;
+    }
+
+
+}
